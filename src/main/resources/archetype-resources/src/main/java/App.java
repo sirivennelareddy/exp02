5b59953b5e3cb2fd78ed49d0ca46e16ea52b471a
Student <Student@225-4>
2025-02-08 12:19:24 +0530

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $devopsexp_2.devops_pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
